96e6dced7de8d4dad3f8bfda1de58c74bd9d34e7
Mirjalo Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-12-23 17:03:58 +0500

    unit of work is completed

--- DotnetEdu.Data/UnitOfWorks/IUnitOfWork.cs
+++ DotnetEdu.Data/UnitOfWorks/IUnitOfWork.cs
@@ -1,5 +1,4 @@
-﻿using DotnetEdu.Data.Contexts;
-using DotnetEdu.Data.Repositores;
+﻿using DotnetEdu.Data.Repositores;
 using DotnetEdu.Domain.Entities;
 
 namespace DotnetEdu.Data.UnitOfWorks;
@@ -8,46 +7,14 @@ public interface IUnitOfWork : IDisposable
 {
     IRepository<Category> Categories { get; }
     IRepository<Course> Courses { get; }
-    IRepository<Module> CourseModules { get; }
-    IRepository<Section> ModuleSections { get; }
-    IRepository<Lesson> SectionLessons { get; }
+    IRepository<Module> Modules { get; }
+    IRepository<Section> Sections { get; }
+    IRepository<Lesson> Lessons { get; }
 
-    IRepository<Article> LessonArticles { get; }
-    IRepository<Quiz> LessonQuiz { get; }
-    IRepository<Exam> LessonExam { get; }
+    IRepository<Article> Articles { get; }
+    IRepository<Quiz> Quiz { get; }
+    IRepository<Exam> Exam { get; }
 
     IRepository<ExamQuestion> ExamQuestions { get; }
     IRepository<QuizQuestion> QuizQuestions { get; }
-}
-public class UnitOfWord : IDisposable
-{
-    public readonly AppDbContext context;
-
-    public UnitOfWord(AppDbContext context)
-    {
-        this.context = context;
-    }
-
-    public IRepository<Category> Categories { get; }
-    public IRepository<Course> Courses { get; }
-    public IRepository<Module> CourseModule { get; }
-    public IRepository<Section> ModuleSection { get; }
-    public IRepository<Lesson> SectionLesson { get; }
-
-    public IRepository<Article> LessonArticles { get; }
-    public IRepository<Quiz> LessonQuiz { get; }
-    public IRepository<Exam> LessonExam { get; }
-
-    public IRepository<ExamQuestion> ExamQuestions { get; }
-    public IRepository<QuizQuestion> QuizQuestions { get; }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-    }
-
-    public async ValueTask<bool> SaveAsync()
-    {
-        return await context.SaveChangesAsync() > 0;
-    }
 }
---
+++ DotnetEdu.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using DotnetEdu.Data.Contexts;
+using DotnetEdu.Data.Repositores;
+using DotnetEdu.Domain.Entities;
+
+namespace DotnetEdu.Data.UnitOfWorks;
+
+public class UnitOfWork : IDisposable
+{
+    public readonly AppDbContext context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public IRepository<Category> Categories { get; }
+    public IRepository<Course> Courses { get; }
+    public IRepository<Module> Module { get; }
+    public IRepository<Section> Section { get; }
+    public IRepository<Lesson> Lesson { get; }
+
+    public IRepository<Article> Articles { get; }
+    public IRepository<Quiz> Quiz { get; }
+    public IRepository<Exam> Exam { get; }
+
+    public IRepository<ExamQuestion> ExamQuestions { get; }
+    public IRepository<QuizQuestion> QuizQuestions { get; }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask<bool> SaveAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
